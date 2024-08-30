4a2fa0b3498fc1239e8e53cb79794ba0074e4397
johhul99 <johan.hultin@iths.se>
2024-08-30 15:56:57 +0200

    V1

---
+++ Laboration-1/Laboration-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.ExceptionServices;
+using static System.Net.Mime.MediaTypeNames;
+
+string main = "12341b323";
+
+
+
+void highlighter (string a)
+{
+
+    int mainLength = a.Length;
+    char indexedChar = a[1];
+    int value = indexedChar;
+
+    for (int i = 2; i < mainLength; i++)
+    { 
+        int highlightLength = i - value + 1;
+        
+        
+
+        if (char.IsLetter(a[i]))
+        {
+            indexedChar = a[i+1];
+        }
+        else if (a[i] == (indexedChar))
+        {
+            string highLighted = a.Substring(value, highlightLength);
+            string nonHighlighted = a.Substring(0, value);
+            string nonHighlighted2 = a.Substring(i+1, highlightLength-i);
+            Console.WriteLine(nonHighlighted);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(highLighted);
+
+            Console.ResetColor();
+            Console.WriteLine(nonHighlighted2);
+
+            indexedChar++;
+        }
+        else
+        {
+            break;
+        }
+
+        
+    }
+}
+
+
+highlighter(main);
