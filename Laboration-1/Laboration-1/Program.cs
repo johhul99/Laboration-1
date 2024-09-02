364fab63dc5e1d78a0d2f565581e4e3bf114c6e7
johhul99 <johan.hultin@iths.se>
2024-09-02 12:50:44 +0200

    v1 och v2 klar

--- Laboration-1/Laboration-1/Program.cs
+++ Laboration-1/Laboration-1/Program.cs
@@ -5,7 +5,7 @@ string main = "29535123p48723487597645723645";
 
 
 
-void highlighter (string a)
+void highlighter (string a) //Highlighta alla tal som börjar och slutar på samma siffra
 {
     int value = 0;
 
@@ -54,4 +54,52 @@ void highlighter (string a)
 }
 
 highlighter(main);
-    
+
+
+Console.WriteLine(""); //Mellanrum mellan resultaten
+
+
+void sum(string a) //Addera alla tal som börjar och slutar på samma siffra
+{
+    int value = 0;
+    long sum = 0;
+
+    for (int i = 1; i < a.Length; i++)
+    {
+        int highlightLength = i - value + 1;
+
+        if (char.IsLetter(a[i]))
+        {
+            value++;
+            i = value;
+
+        }
+        else if (char.IsLetter(a[value]))
+        {
+            value++;
+            i = value;
+        }
+        else if (a[i] == a[value])
+        {
+            string temp = a.Substring(value, highlightLength);
+            long add = long.Parse (temp);
+            sum += add;
+
+            value++;
+            i = value;
+        }
+        else if (i == a.Length - 1)
+        {
+            value++;
+            i = value;
+        }
+        else
+        {
+
+        }
+        
+    }
+    Console.WriteLine(sum);
+}
+
+sum(main);
